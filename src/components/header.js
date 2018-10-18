6c50a5f50eb8e98713f40109285bad2d912b8a04
User@Desktop-3D4BBRS <kebrew2@gmail.com>
2018-10-18 18:50:30 -0400

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="title">
+        <h1>My First React App</h1>
+        <p>Need to come up with an idea</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
